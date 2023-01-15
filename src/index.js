fb4604923724fe44887bd9ae06003e0ac397e51d
Yulia-Safonova-85 <109626421+Yulia-Safonova-85@users.noreply.github.com>
2023-01-15 21:20:46 +0200

    remove file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './css/styles.css';
+
+// import debounce from "lodash.debounce";
+
+// import { fetchCountries } from './fetchCountries.js';
+
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
